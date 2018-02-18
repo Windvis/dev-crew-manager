cc8ac06e0f68d7ab5b5968e974e080c2d6ffcfa2
Sam Van Campenhout <sam.van.campenhout@kunstmaan.be>
2018-02-18 18:59:04 +0100

    Base scss structure + styling
    Also added normalize.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import './index.css'
+import '../src/assets/css/app.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { store } from './redux/store'
