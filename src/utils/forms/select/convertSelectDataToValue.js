de2c426dcf29bbc9dd3fd76805739d3520b1fa0d
Sam Van Campenhout <sam.van.campenhout@kunstmaan.be>
2018-03-09 09:14:51 +0100

    Fix a problem where the selectfield threw an exception when clearing the field

--- src/utils/forms/select/convertSelectDataToValue.js
+++ src/utils/forms/select/convertSelectDataToValue.js
@@ -4,7 +4,9 @@ export function convertSelectDataToValue (selectData) {
   if (is.array(selectData)) {
     // Multi select, return an array of values
     return selectData.map((data) => data.value)
+  } else if (!is.null(selectData) && is.object(selectData)) {
+    return selectData.value
   }
-
-  return selectData.value
+  // React-Select returns null when the field is cleared
+  return selectData
 }
